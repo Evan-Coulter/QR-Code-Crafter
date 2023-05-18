c79e172a9388021367151378047ebac9416ff2b3
Evan-Coulter <evanrileycoulter@gmail.com>
2023-05-18 14:12:14 -0700

    add text input styling

--- src/scenes/textinput/index.tsx
+++ src/scenes/textinput/index.tsx
@@ -5,8 +5,13 @@ type Props = {
 
 const TextInput = ({onTextChange, initialText}: Props) => {
   return (
-    <section id="text-input">
-      <input name="qrCodeInput" value={initialText} onChange={e=>{onTextChange(e.target.value)}}/>
+    <section id="text-input" className="max-w-[70%] h-[200px] flex mx-auto mt-8"> 
+      <textarea 
+        rows={1}
+        name="qrCodeInput" 
+        value={initialText} 
+        onChange={e=>{onTextChange(e.target.value)}}
+        className="m-auto resize-none h-full w-full shadow appearance-none border-[2px] border-secondary rounded-[8px] bg-transparent py-4 px-5 text-offWhite text-xl leading-tight focus:outline-none focus:shadow-outline focus:border-secondaryLight"/>
     </section>
   )
 }
